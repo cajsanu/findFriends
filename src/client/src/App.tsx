c89bb85fa3b3af09038b35ddaea1cf4b4179d88d
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-07-18 13:04:46 +0300

    Add typed and models for user and dog

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,23 +1,24 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [res, setRes] = useState<string | null>(null)
+  const [res, setRes] = useState<string | null>(null);
 
-useEffect(() => {
-  const ping = async() => {
-    const res = await axios.get<string>("http://localhost:5053/api/ping")
-    setRes(res.data)
-  }
-  ping()
-}, [])
+  useEffect(() => {
+    const ping = async () => {
+      const res = await axios.get<string>("http://localhost:5053/api/ping");
+      setRes(res.data);
+    };
+    ping();
+  }, []);
 
   return (
     <>
-     <p>{res}</p>
+      <h1>Find Friends</h1>
+      <p>{res}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/server/obj/Debug/net8.0/findFriends.AssemblyInfo.cs
+++ src/server/obj/Debug/net8.0/findFriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f3c44fbdcef14bfa59cff2d0c30fa46b7b12b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba254c384af7c9c2e15ca34de87caa571e2a2f47")]
 [assembly: System.Reflection.AssemblyProductAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
