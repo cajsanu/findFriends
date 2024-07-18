ddc68b0b2e6c7bdc8f0fcf00283b1f3520677d40
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-07-18 09:53:34 +0300

    Initialise project

---
+++ src/server/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/", () =>
+{
+    return "Hello World!";
+})
+.WithName("Hello world")
+.WithOpenApi();
+
+app.Run();
+
