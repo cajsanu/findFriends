64810bf6f1687b6a3cf394b15457cfc9fdc12093
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-07-18 16:41:23 +0300

    Add singledog component

--- src/server/obj/Debug/net8.0/findFriends.AssemblyInfo.cs
+++ src/server/obj/Debug/net8.0/findFriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc1e9366bf2ee5323b2de2a1632662435c46327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f34b0b91c12fbe57c745c5a46e765e9fb0c5b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
