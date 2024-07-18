573d59adceb66017145d9d4a0d4bc7137670afd3
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-07-18 11:00:19 +0300

    Initialise frontend

--- src/server/obj/Debug/net8.0/findFriends.AssemblyInfo.cs
+++ src/server/obj/Debug/net8.0/findFriends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf5550a9c6058b1bc0638442716346f1a64a1f5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3710e85eda8259cc11db5be836d9104667b137")]
 [assembly: System.Reflection.AssemblyProductAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("findFriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
