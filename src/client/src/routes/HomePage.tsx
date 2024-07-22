11540c181438e9fb5d85d40876f7f4a723c0323f
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-07-22 12:37:29 +0300

    Make homepage fetch random dog facts

--- src/client/src/routes/HomePage.tsx
+++ src/client/src/routes/HomePage.tsx
@@ -1,27 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
+  const [fact, setFact] = useState(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const getDogFact = async() => {
+        const res = await axios.get("https://dogapi.dog/api/facts")
+        setFact(res.data.facts)
+    };
+    getDogFact();
+  }, []);
 
   const handleClick = () => {
     navigate("/users");
   };
 
   return (
-    <div className="felx justify-center">
-      <div className="bg-sky-300">
-        <h1 className="text-sky-500">Find Friends</h1>
+    <div className="felx flex-col justify-center p-10">
+      <div className="bg-sky-300 p-5">
+        <h1 className="text-sky-700">Welcome user</h1>
         <button
-          className="rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-200 hover:text-teal-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+          className="rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-200 hover:text-pink-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
           onClick={handleClick}
         >
           See users
         </button>
+        <button
+          className="rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-200 hover:text-pink-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
+          onClick={handleClick}
+        >
+          Start new chat
+        </button>
       </div>
-      <div className="flex justify-end">
+      <div className="flex flex-row justify-around p-10">
         <div className="flex flex-col">
-          <p>Did you know?</p>
-          <p className="bg-white text-black rounded p-5">DOG FACTS HERE</p>
+          <p className="bg-white">All chats of user</p>
+          <div className="bg-pink-200 p-10">
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Dogs</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Name</td>
+                  <td>Dogs</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <div className="">
+            <p>Did you know?</p>
+            <p className="max-w-56 bg-white text-black rounded p-5">{fact}</p>
+          </div>
         </div>
       </div>
     </div>
--- src/client/src/routes/SignInPage.tsx
+++ src/client/src/routes/SignInPage.tsx
@@ -1,10 +1,22 @@
-
-
-
 export const SignInPage = () => {
-    return (
+  return (
+    <div>
+      <div className="flex flex-col">
         <div>
-            Sign In
+          <h1 className="p-10">Welcome to FindFriends</h1>
         </div>
-    )
-}
+        <div className="flex flex-row justify-center justify-around">
+          <div>
+            <p>This would include a short description</p>
+          </div>
+          <div>
+            <p>And here would be the sign in form</p>
+          </div>
+        </div>
+        <div className="">
+          <p>This should be at the bottom</p>
+        </div>
+      </div>
+    </div>
+  );
+};
