96b838e4ade2e23746b183d3ac2ec0846bf48e8e
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-08-06 10:06:27 +0300

    Add user token to localstorage on login

--- src/server/Services/UserService.cs
+++ src/server/Services/UserService.cs
@@ -28,16 +28,9 @@ public class UserService(FindFriendsContext context, UserManager<User> userManag
 
             var userId = _contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            Console.WriteLine(userId);
-            foreach (var claim in _contextAccessor.HttpContext.User.Claims)
-            {
-                Console.WriteLine($"{claim.Type}: {claim.Value}");
-            }
-
             if (userId != null)
             {
                 var user = await _context.Users.FindAsync(userId);
-                Console.WriteLine(user + "!!!!!!!");
                 return user;
             }
         }
