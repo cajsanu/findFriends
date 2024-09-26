04ef6b24e5ddf76deaa992163ed2fcdf08c374ee
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-09-26 10:11:12 +0300

    Add zod validation for loginForm

--- src/client/src/components/LoginForm.tsx
+++ src/client/src/components/LoginForm.tsx
@@ -2,14 +2,22 @@ import { login } from "../requests/login";
 import { useNavigate } from "react-router-dom";
 import { getCurrentUser } from "../requests/user";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type LoginFields = {
-  email: string;
-  password: string;
-};
+const schema = z.object({
+  email: z.string().email({ message: "Please provide a valid email" }),
+  password: z.string().min(10),
+});
+
+type LoginFields = z.infer<typeof schema>;
 
 export const LoginForm = () => {
-  const { register, handleSubmit } = useForm<LoginFields>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFields>({ resolver: zodResolver(schema) });
   const navigate = useNavigate();
 
   const onSubmit: SubmitHandler<LoginFields> = async ({ email, password }) => {
@@ -37,10 +45,15 @@ export const LoginForm = () => {
                 Email:
               </label>
               <input
-                type="email"
+                type="text"
                 {...register("email")}
                 className="block w-full rounded-md py-1.5 text-white outline outline-transparent focus:outline-pink-800"
               />
+              {errors.email && (
+                <div className="p-2">
+                  <p className="text-black">{errors.email.message}</p>
+                </div>
+              )}
             </div>
             <div>
               <label className="block text-sm font-medium leading-6 text-white flex justify-right pt-2">
@@ -51,6 +64,9 @@ export const LoginForm = () => {
                 {...register("password")}
                 className="block w-full rounded-md py-1.5 text-white outline outline-transparent focus:outline-pink-800"
               />
+              {errors.password && (
+                <div className="text-red-500">{errors.password.message}</div>
+              )}
             </div>
             <div className="py-3">
               <button
